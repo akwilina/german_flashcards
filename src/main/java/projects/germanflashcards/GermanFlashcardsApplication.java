24451b77990e7182c22bf21835cd056f1d221b48
delola\delola lokal <aleksandra.man@gmail.com>
2021-01-28 18:32:35 +0100

    structure :: initialproject structure

---
+++ src/main/java/projects/germanflashcards/GermanFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package projects.germanflashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermanFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermanFlashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/germanflashcards/GermanFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.germanflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermanFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
