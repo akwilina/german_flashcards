86f626cc176a47c200b2c1f45aaff52cbf1de911
delola\delola lokal <aleksandra.man@gmail.com>
2021-03-20 22:29:58 +0100

    register-user :: security configuration

--- src/main/java/projects/germanflashcards/domain/UserRepository.java
+++ src/main/java/projects/germanflashcards/domain/UserRepository.java
@@ -3,6 +3,10 @@ package projects.germanflashcards.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projects.germanflashcards.domain.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
