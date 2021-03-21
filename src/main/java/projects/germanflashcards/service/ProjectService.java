aada16fb4c1df32e425aa5cf52e3696b2b4d0a60
delola\delola lokal <aleksandra.man@gmail.com>
2021-03-21 16:19:06 +0100

    add-project :: controller update and handling POST request

--- src/main/java/projects/germanflashcards/service/ProjectService.java
+++ src/main/java/projects/germanflashcards/service/ProjectService.java
@@ -1,4 +1,15 @@
 package projects.germanflashcards.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import projects.germanflashcards.web.command.CreateProjectCommand;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProjectService {
+    public void add(CreateProjectCommand createProjectCommand) {
+
+    }
 }
--- src/main/java/projects/germanflashcards/web/controller/AddNewProjectController.java
+++ src/main/java/projects/germanflashcards/web/controller/AddNewProjectController.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import projects.germanflashcards.service.ProjectService;
 import projects.germanflashcards.web.command.CreateProjectCommand;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/projects")
@@ -14,10 +19,33 @@ import projects.germanflashcards.web.command.CreateProjectCommand;
 @RequiredArgsConstructor
 public class AddNewProjectController {
 
+    private final ProjectService projectService;
+
     @GetMapping("/add")
     public String getAddProjectPage(Model model) {
         model.addAttribute(new CreateProjectCommand());
         return "project/add";
     }
 
+    @PostMapping
+    public String processAddProject(@Valid CreateProjectCommand createProjectCommand, BindingResult bindings) {
+        log.debug("Data for project creation: {}", createProjectCommand);
+
+        if (bindings.hasErrors()) {
+            log.debug("Data has errors: {}", bindings.getAllErrors());
+            return "project/add";
+        }
+
+        try {
+            projectService.add(createProjectCommand);
+            log.debug("Project has been created");
+            return "redirect:/projects";
+
+        } catch (RuntimeException re) {
+            log.warn(re.getLocalizedMessage());
+            log.debug("Error while project creation", re);
+            bindings.rejectValue(null, null, "Error occured");
+            return "project/add";
+        }
+    }
 }
