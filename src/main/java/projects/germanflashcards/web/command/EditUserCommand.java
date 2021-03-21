bc9d8f58634af602cb548a68abf3acd0fb07fd8d
delola\delola lokal <aleksandra.man@gmail.com>
2021-03-21 13:51:23 +0100

    register-user :: template for controller data edition

---
+++ src/main/java/projects/germanflashcards/web/command/EditUserCommand.java
@@ -0,0 +1,26 @@
+package projects.germanflashcards.web.command;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EditUserCommand {
+
+    @Size(max = 64)
+    private String firstName;
+    @Size(max = 64)
+    private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private LocalDate birthDate;
+}
--- src/main/java/projects/germanflashcards/web/controller/UserProfileController.java
+++ src/main/java/projects/germanflashcards/web/controller/UserProfileController.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import projects.germanflashcards.data.user.UserSummary;
 import projects.germanflashcards.service.UserService;
+import projects.germanflashcards.web.command.EditUserCommand;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/profile")
@@ -20,8 +25,26 @@ public class UserProfileController {
     @GetMapping
     public String getProfilePage(Model model) {
         UserSummary summary = userService.getCurrentUserSummaty();
-        model.addAttribute("userSummary", summary);
+        EditUserCommand editUserCommand = createEditUserCommand(summary);
+        model.addAttribute(summary);
+        model.addAttribute(editUserCommand);
         return "user/profile";
+    }
+
+    private EditUserCommand createEditUserCommand(UserSummary summary) {
+        return EditUserCommand.builder()
+                .firstName(summary.getFirstName())
+                .lastName(summary.getLastName())
+                .birthDate(summary.getBirthDate())
+                .build();
+    }
+
+    @PostMapping("/edit")
+    public String editUserProfile(@Valid EditUserCommand editUserCommand, BindingResult bindings) {
+        //TODO
+
+        return "redirect:/profile";
+
     }
 
 }
