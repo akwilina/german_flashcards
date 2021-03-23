a9b157bdf11eabec3a00632a85305ac498422a1a
akwilina <aleksandra.man@gmail.com>
2021-03-23 18:41:05 +0100

    structure :: application renaming

--- src/test/java/projects/germanflashcards/GermanFlashcardsApplicationTests.java
+++ src/test/java/projects/germanflashcards/GermanFlashcardsApplicationTests.java
@@ -1,10 +1,10 @@
-package projects.germanflashcards;
+package projects.projectmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GermanFlashcardsApplicationTests {
+class ProjectManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
